422d8b7f893c62285cdfdafed27a488dfa162e20
Iconians <claytoncripe@gmail.com>
2025-02-05 14:32:24 -0800

    finished the easy.ts problems

--- problems/easy.ts
+++ problems/easy.ts
@@ -10,6 +10,23 @@
  *
  */
 
+const twoSum = (arr: number[], sum: number): number[] => {
+  const map = new Map();
+
+  for (let i = 0; i < arr.length; i++) {
+    let num = sum - arr[i];
+    if (map.has(num)) {
+      return [map.get(num), i];
+    }
+    map.set(arr[i], i);
+  }
+  return [];
+};
+
+// console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
+// console.log(twoSum([3, 2, 4], 6)); // [1, 2]
+// console.log(twoSum([3, 3], 6)); // [0, 1]
+// console.log(twoSum([3, 2, 3], 6)); // [0, 2]
 /*
  * Problem: Reverse Words in a String
  *
@@ -20,7 +37,27 @@
  * Output: "blue is sky the"
  *
  */
+const reverseStr1 = (str: string): string => str.split(" ").reverse().join(" ");
 
+const reverseStr2 = (str: string): string => {
+  let reversed = "";
+  const split = str.split(" ");
+
+  for (let i = split.length - 1; i >= 0; i--) {
+    if (i === 0) {
+      reversed += split[i];
+    } else {
+      reversed += split[i] + " ";
+    }
+  }
+
+  return reversed;
+};
+
+// console.log(reverseStr1("the sky is blue"));
+// console.log(reverseStr2("the sky is blue"));
+// console.log(reverseStr1("Reverse Words in a String"));
+// console.log(reverseStr2("Reverse Words in a String"));
 /*
  * Problem: Most Common Character
  *
@@ -31,6 +68,30 @@
  * Output: "a"
  *
  */
+
+const mostCommonChar = (str: string): string => {
+  const map = new Map();
+  let mostCommon = "";
+  let count = 0;
+
+  for (let char of str) {
+    if (map.has(char)) {
+      map.set(char, map.get(char) + 1);
+    } else map.set(char, 1);
+  }
+
+  for (let [key, value] of map.entries()) {
+    if (value > count) {
+      count = value;
+      mostCommon = key;
+    }
+  }
+
+  return mostCommon;
+};
+
+// console.log(mostCommonChar("banana"));
+// console.log(mostCommonChar("Problem: Most Common Character"));
 
 /*
  * Problem: Find Duplicates
@@ -43,6 +104,30 @@
  *
  */
 
+const findDuplicates = (arr: number[]): number[] => {
+  const map = new Map();
+  let returnArr = [];
+
+  for (let num of arr) {
+    if (map.has(num)) {
+      map.set(num, map.get(num) + 1);
+    } else map.set(num, 1);
+  }
+
+  for (let [key, value] of map.entries()) {
+    if (value > 1) {
+      returnArr.push(key);
+    }
+  }
+  return returnArr;
+};
+
+console.log(findDuplicates([4, 3, 2, 7, 8, 2, 3, 1])); // Expected output: [2, 3] but because of map it returns [3, 2]
+console.log(findDuplicates([1, 2, 3, 4, 5, 6, 1, 2])); // Expected output: [1, 2]
+console.log(findDuplicates([10, 20, 30, 40, 50])); // Expected output: []
+console.log(findDuplicates([5, 5, 5, 5, 5])); // Expected output: [5]
+console.log(findDuplicates([1, 2, 3, 4, 4, 5, 6, 7, 8, 9, 9, 10])); // Expected output: [4, 9]
+
 /*
  * Problem: First Unique Character
  *
@@ -53,6 +138,22 @@
  * Output: 0
  *
  */
+
+const fistUniqueChar = (str: string): number => {
+  const map = new Map();
+  let index = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[i + 1] && !map.has(str[i])) {
+      return i;
+    } else map.set(str[i], 1);
+  }
+  return index;
+};
+
+// console.log(fistUniqueChar("leetcode"));
+// console.log(fistUniqueChar("aabbc"));
+// console.log(fistUniqueChar("aaaaaaazaa"));
 
 /*
  * Problem: Find All Duplicates in an Array
