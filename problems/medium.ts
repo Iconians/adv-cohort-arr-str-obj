c4196dd41b6e954fc6b68d613268f7d35cec6930
Iconians <claytoncripe@gmail.com>
2025-02-05 17:25:57 -0800

    did four of the six problems in the medium.ts

--- problems/medium.ts
+++ problems/medium.ts
@@ -9,6 +9,41 @@
  *
  */
 
+const mergeIntervals = (arr: number[][]): number[][] => {
+  arr.sort((a, b) => a[0] - b[0]);
+  let newArr: number[][] = [arr[0]];
+
+  for (let i = 1; i < arr.length; i++) {
+    let subArr = arr[i];
+    let merged = newArr[newArr.length - 1];
+
+    if (subArr[0] <= merged[1]) {
+      merged[1] = Math.max(merged[1], subArr[1]);
+    } else {
+      newArr.push(subArr);
+    }
+  }
+
+  return newArr;
+};
+
+// console.log(
+//   mergeIntervals([
+//     [1, 3],
+//     [2, 6],
+//     [8, 10],
+//     [15, 18],
+//   ])
+// ); // [[1,6],[8,10],[15,18]]
+// console.log(
+//   mergeIntervals([
+//     [1, 3],
+//     [15, 18],
+//     [2, 6],
+//     [8, 10],
+//   ])
+// ); // [[1,6],[8,10],[15,18]]
+
 /*
  * Problem: Group Anagrams
  *
@@ -19,6 +54,30 @@
  * Output: [["eat","tea","ate"],["tan","nat"],["bat"]]
  *
  */
+
+const groupAnagrams = (arr: string[]) => {
+  const map = new Map();
+
+  for (let str of arr) {
+    const sort = str.split("").sort().join("");
+    if (map.has(sort)) {
+      map.get(sort).push(str);
+    } else {
+      map.set(sort, [str]);
+    }
+  }
+  return Array.from(map.values());
+};
+
+// console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
+// output
+/*
+[
+  ["eat", "tea", "ate"],
+  ["tan", "nat"],
+  ["bat"]
+]
+*/
 
 /*
  * Problem: Longest Palindromic Substring
@@ -31,6 +90,37 @@
  *
  */
 
+("I modified a function I did for a different palindrome algo");
+
+const isPalindrome = (str: string, left: number, right: number) => {
+  while (left >= 0 && right < str.length && str[left] === str[right]) {
+    left--;
+    right++;
+  }
+  return str.slice(left + 1, right);
+};
+
+const longestPalindrome = (str: string): string => {
+  let longest = "";
+
+  for (let i = 0; i < str.length; i++) {
+    let odd = isPalindrome(str, i, i);
+    let even = isPalindrome(str, i, i + 1);
+    if (odd.length > longest.length) longest = odd;
+    if (even.length > longest.length) longest = even;
+  }
+
+  return longest;
+};
+
+// console.log(longestPalindrome("babad")); // Output: "bab" or "aba"
+// console.log(longestPalindrome("cbbd")); // Output: "bb"
+// console.log(longestPalindrome("a")); // Output: "a"
+// console.log(longestPalindrome("ac")); // Output: "a" or "c"
+// console.log(longestPalindrome("racecar")); // Output: "racecar"
+// console.log(longestPalindrome("")); // Output: ""
+// console.log(longestPalindrome("abacdfgdcaba")); // Output: "aba" or "aca"
+
 /*
  * Problem: Flatten Nested Object
  *
@@ -42,6 +132,22 @@
  *
  * Hint: Use recursion + a helper function.
  */
+
+const flattenNestedObject = (
+  obj: { [key: string]: any },
+  str = "",
+  flatObj: { [key: string]: any } = {}
+): object => {
+  for (let key in obj) {
+    if (typeof obj[key] === "object") {
+      flattenNestedObject(obj[key], `${str}${key}.`, flatObj);
+    } else flatObj[`${str}${key}`] = obj[key];
+  }
+
+  return flatObj;
+};
+
+console.log(flattenNestedObject({ a: { b: { c: 1 } }, d: 2 }));
 
 /*
  * Problem: Deep Object Comparison
